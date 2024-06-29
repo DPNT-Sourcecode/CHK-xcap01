c69a2edafcef697e91793a6934dd9dc64d9530f7
sbx_user1051 <sbx_user1051@169.254.80.21>
2024-06-29 17:24:39 +0000

    Sat Jun 29 17:24:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,15 +3,14 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    prices = []
-    prices.append('A', 50, True)
-    prices.append('B', 30, True)
-    prices.append('C', 20, False)
-    prices.append('D', 15, False)
+    prices = {'A', 50, 'B', 30, 'C', 20, 'D', 15};
 
     total = 0
     basket = skus.split()
     for sku in basket:
+        total += prices.get(sku)
+
+    return total
 
 
 
