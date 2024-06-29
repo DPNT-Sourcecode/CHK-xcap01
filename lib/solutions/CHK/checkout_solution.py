f2ad61b523948c88b555a091179f251a3ed71f28
sbx_user1051 <sbx_user1051@169.254.80.21>
2024-06-29 17:39:39 +0000

    Sat Jun 29 17:39:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,3 +1,19 @@
+from collections import OrderedDict
+
+class PricingRules(object):
+    def __init__(self):
+        self._rules = {}
+
+    def add_rule(self, item, price, quantity=1):
+        if quantity not in self._rules:
+            self._rules[quantity] = {}
+
+        self._rules[quantity][item] = price
+
+    @property
+    def rules(self):
+        return self._rules
+
 
 
 # noinspection PyUnusedLocal
@@ -9,11 +25,13 @@ def checkout(skus):
     basket = skus.split()
     for sku in basket:
         try:
+            if (item[])
             total += prices[sku]
         except:
             return -1
 
     return total
+
 
 
 
