745f33a88b099e4a892f55ae42a508445b36b3a3
sbx_user1051 <sbx_user1051@169.254.80.21>
2024-06-29 17:30:39 +0000

    Sat Jun 29 17:30:39 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    prices = {'A', 50, 'B', 30, 'C', 20, 'D', 15}
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
 
     total = 0
     basket = skus.split()
     for sku in basket:
-        total += prices.get(sku, 0)
+        try:
+            total += prices[sku]
+        except:
+            return -1
 
     return total
+
--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -2,6 +2,10 @@ from lib.solutions.CHK import checkout_solution
 
 
 class TestCheckout():
-    def test_checkout(self):
+    def test_simple_checkout(self):
         assert checkout_solution.checkout('A B C') == 100
+
+    def test_invalid_item(self):
+        assert checkout_solution.checkout('A B E') == -1
+
 
