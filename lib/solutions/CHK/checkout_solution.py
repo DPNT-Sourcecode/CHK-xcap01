40f4903e051ab8577d6875da89b2d3545d862e2d
sbx_user1051 <sbx_user1051@169.254.80.21>
2024-06-29 21:40:01 +0000

    Sat Jun 29 21:40:01 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,31 +6,6 @@ class PricingRules(object):
     def __init__(self):
         self._rules = {}
         self._given_rules = '''
-            | A    | 50    | 3A for 130, 5A for 200          |
-            | B    | 30    | 2B for 45                       |
-            | C    | 20    |                                 |
-            | D    | 15    |                                 |
-            | E    | 40    | 2E get one B free               |
-            | F    | 10    | 2F get one F free               |
-            | G    | 20    |                                 |
-            | H    | 10    | 5H for 45, 10H for 80           |
-            | I    | 35    |                                 |
-            | J    | 60    |                                 |
-            | K    | 70    | 2K for 120                      |
-            | L    | 90    |                                 |
-            | M    | 15    |                                 |
-            | N    | 40    | 3N get one M free               |
-            | O    | 10    |                                 |
-            | P    | 50    | 5P for 200                      |
-            | Q    | 30    | 3Q for 80                       |
-            | R    | 50    | 3R get one Q free               |
-            | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-            | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
-            | U    | 40    | 3U get one U free               |
-            | V    | 50    | 2V for 90, 3V for 130           |
-            | W    | 20    |                                 |
-            | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
-            | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
             | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |'''
 
     def __add_rule(self, item, price, quantity=1, free_item=''):
@@ -42,7 +17,9 @@ class PricingRules(object):
     def __process_special_offers(self, sku, unit_price, offer_detail):
         for offer in offer_detail.split(','):
             offer = offer.strip()
-            if "for" in offer:
+            if "buy any" in offer:
+                here = True
+            elif "for" in offer:
                 self.__add_rule(offer[1], int(offer[7:]), int(offer[0]))
             elif "get one" in offer:
                 if offer[11] == sku:
@@ -146,3 +123,4 @@ def calculate_basket_cost(skus, apply_discount):
         return calculate_basket_cost(updated_basket, False)
 
     return price
+
