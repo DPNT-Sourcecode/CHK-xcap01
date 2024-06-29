95c316a86f03709f13f266050e2f4530fce7cf34
sbx_user1051 <sbx_user1051@169.254.80.21>
2024-06-29 20:14:43 +0000

    Sat Jun 29 20:14:43 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -28,13 +28,27 @@ price_rules.add_rule('A', 50)
 price_rules.add_rule('E', 40)
 price_rules.add_rule('F', 10)
 price_rules.add_rule('G', 20)
+price_rules.add_rule('H', 10)
+price_rules.add_rule('I', 35)
+price_rules.add_rule('J', 60)
+price_rules.add_rule('K', 80)
+price_rules.add_rule('L', 90)
+price_rules.add_rule('M', 15)
+price_rules.add_rule('N', 40)
+price_rules.add_rule('O', 10)
+price_rules.add_rule('P', 50)
+price_rules.add_rule('Q', 30)
 price_rules.add_rule('A', 130, 3)
 price_rules.add_rule('A', 200, 5)
 price_rules.add_rule('B', 45, 2)
 price_rules.add_rule('E', 80, 2, 'B')
 price_rules.add_rule('F', 20, 3)
-
-
+price_rules.add_rule('H', 45, 5)
+price_rules.add_rule('H', 80, 10)
+price_rules.add_rule('K', 150, 2)
+price_rules.add_rule('N', 120, 3, 'M')
+price_rules.add_rule('P', 200, 5)
+price_rules.add_rule('Q', 80, 3)
 # skus = unicode string
 def checkout(skus):
     return calculate_basket_cost(skus, True)
@@ -65,3 +79,4 @@ def calculate_basket_cost(skus, apply_discount):
         return calculate_basket_cost(updated_basket, False)
 
     return price
+
